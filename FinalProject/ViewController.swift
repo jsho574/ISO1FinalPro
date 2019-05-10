ff50509cfc3fdee6a04b8656502316c230636469
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-05-10 11:44:32 -0500

    basic build complete

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -9,7 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numbOnScreen:Double = 0;
+    var storeNumb:Double = 0;
+    var performing = false
+    var operation = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func Numbers(_ sender: UIButton) {
+        if performing == true {
+            label.text = String(sender.tag-1)
+            numbOnScreen = Double(label.text!)!
+            performing = false
+        }
+        else{
+            label.text = label.text! + String(sender.tag-1)
+            numbOnScreen = Double(label.text!)!
+        }
+        
+        
+        
+        
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+            storeNumb = numbOnScreen
+            switch sender.tag {
+            case 12://Divide
+                label.text = "/"
+            case 13://multiply
+                label.text = "x"
+            case 14://subtract
+                label.text = "-"
+            case 15://add
+                label.text = "+"
+            default:
+                label.text = ""
+            }
+            operation = sender.tag
+            performing = true
+        }
+        else if sender.tag == 16{
+            switch operation{
+            case 12:
+                numbOnScreen = storeNumb / numbOnScreen
+            case 13:
+                numbOnScreen = storeNumb * numbOnScreen
+            case 14:
+                numbOnScreen = storeNumb - numbOnScreen
+            case 15:
+                numbOnScreen = storeNumb + numbOnScreen
+            default:
+                numbOnScreen = storeNumb
+            }
+            label.text = String(numbOnScreen)
+            
+        }
+        else {
+            label.text = ""
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
